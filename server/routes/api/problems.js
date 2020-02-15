83507b6849e68161a432cfd1751c85300d42b487
Steffan <sachtmann121@gmail.com>
2020-02-15 14:32:56 -0800

    Updates checks for problem post, also adds a problem update (put) request.

--- server/routes/api/problems.js
+++ server/routes/api/problems.js
@@ -35,7 +35,9 @@ router.get('/:id', async (req, res) => {
 // @access Admin
 router.post('/', [auth, [
     check('id', 'Problem must have valid id.').not().isEmpty(),
-
+    check('name', 'Problem must have a name').not().isEmpty(),
+    check('problem_text', 'Problem must have accompanying text').not().isEmpty(),
+    check('link', 'Must include link to problem').not().isEmpty(),
 ]], async (req, res) => {
     // Check our request contains required fields
     const validationErrors = validationResult(req)
@@ -81,6 +83,62 @@ router.post('/', [auth, [
         // Send to DB
         const problem = await newProblem.save()
         return res.json(problem)
+    } catch (error) {
+        console.error(error.message)
+        return res.status(500).send('Server error.')
+    }
+})
+
+
+// @route  PUT /api/problems
+// @desc   Updates a LeetCode problem in the database
+// @access Admin
+router.put('/', [auth, [
+    check('id', 'Problem must have valid id.').not().isEmpty(),
+]], async (req, res) => {
+    // Check our request contains required fields
+    const validationErrors = validationResult(req)
+    if (!validationErrors.isEmpty) {
+        // Something was missing, send an error
+        return res.status(400).json({errors : errors.array()})
+    }
+    try {
+        // Get user, ensure is admin user.
+        //TODO: Learn the real way to make admin routes
+        // If admin, allow. If not, unauthorized msg
+        const user = await User.findById(req.user.id)
+        const admin_email = process.env.ADMIN_EMAIL
+        if (!user || user.email != admin_email) {
+            return res.status(401).json({msg: 'Access denied'})
+        }
+
+        // Valid admin - parse out the updated fields
+        const {
+            id,
+            name,
+            problem_text,
+            link,
+            test_case,
+            start_code,
+        } = req.body
+
+        // Find the problem to be updated
+        const existingProblem = await Problem.findOne({id: id})
+        if (!existingProblem) {
+            // Couldn't find the problem, send Conflict response
+            return res.status(409).json({errors: [{msg: 'Problem does not exist.'}]})
+        }
+        // Update the problem's existing fields with updated ones, if the updated exist
+        // Note we do not/cannot allow changes to the id field
+        if (name) {existingProblem.name = name}
+        if (problem_text) {existingProblem.problem_text = problem_text}
+        if (link) {existingProblem.link = link}
+        if (test_case) {existingProblem.test_case = test_case}
+        if (start_code) {existingProblem.start_code = start_code}
+
+        // Send the update to the database
+        const updatedProblem = await existingProblem.save()
+        return res.json(updatedProblem)
     } catch (error) {
         console.error(error.message)
         return res.status(500).send('Server error.')
