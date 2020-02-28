1a4512d6db6cec971b807e25e6525ffd6acb9363
Steffan <sachtmann121@gmail.com>
2020-02-28 04:02:44 +0000

    Added some layout notes, some skeleton work for the eventual selector component

--- leetcode-srs/src/containers/MainPage/MainPage.js
+++ leetcode-srs/src/containers/MainPage/MainPage.js
@@ -1,8 +1,12 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 // import {Link} from 'react-router-dom'
 // import PropTypes from 'prop-types'
 import classes from './MainPage.module.css'
 import { connect } from 'react-redux'
+
+import Selector from './Selector/Selector'
+import Button from '../UI/Button/Button'
+import Input from '../UI/Input/Input'
 
 /*
 Main Page is made of 3 main components:
@@ -15,39 +19,37 @@ const MainPage = (props) => {
         console.log("Test")
     }, [])
 
+    const [elements, setelements] = useState({
+        formVisible: false,
+        timerVisible: false,
+        currentProblemStub: '',
+    })
+
 
     const link = 'https://leetcode.com/problems/' // + props.problem
+    let form = null
 
     const openProblemHandler = (event) => {
-        //TODO: Start the timer object's timer here.
-        // Thoughts on the flow:
-        /*
-            Set some 'problem started' redux state
-            Use said state to refresh main page to include timer
-            Include result box
-        This is all nice-to-have, but I should just get the base flow working first
-        */
+        // Open the submission form
+
+        // Open the timer box and start the timer
         console.log('Problem started')
     }
 
-    /*
-    Thoughts on where this could go now that iframes are out
-        - https://github.com/skygragon/leetcode-cli/blob/master/lib/core.js
-        - Seeing as there's a CLI version, it seems do-able to split this into a couple aspects:
-            1) Show the problem question
-            2) Provide a space for test cases
-            3) Allow user to hit run
-            4) Allow user to hit submit
-
-            On submit, check if success or fail. Stop timer, record result, record the submission, store all
-                Store as fail at first, allow manual override. Don't delete anything from page, just setup
-            
-            This kind of stuff depends a lot on being able to basically re-do the work done in the CLI version.
-                Might be able to borrow some code, or worst case use for inspiration on acccessing LC
-    */
+    const updateProblem = () => {
+        // We need to grab the next problem from the list and:
+        /*
+        Update the link
+        Close the form
+        Close the timer
+        Reset our timer and form (depending on how we deal w/ closing)
+        */
+    }
 
     return (
         <div className={classes.MainPage}>
+            <Selector/>
+            
             <a href={link} target='_blank' rel="noopener noreferrer" onClick={openProblemHandler}>Start Problem</a>
         </div>
     )
@@ -55,7 +57,8 @@ const MainPage = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        
+        curProblem: state.curProblem, // currently selected problem
+        curList: state.curList, // currently selected list
     }
 }
 
