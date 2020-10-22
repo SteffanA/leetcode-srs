b41adfa35fa995a97e45bd32008e2e9e6624649c
Steffan <sachtmann121@gmail.com>
2020-10-21 21:44:58 -0700

    Quick commit - starting work on a form to do problem searching.

--- leetcode-srs/src/containers/ProblemViewer/ProblemViewer.js
+++ leetcode-srs/src/containers/ProblemViewer/ProblemViewer.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import * as problemActions from '../../store/actions/problems'
@@ -11,13 +11,28 @@ export const ProblemViewer = (props) => {
         getProblemSubset
     } = props
 
+    const [
+        searchTerm,
+        setSearchTerm
+    ] = useState('Search for a Problem')
+
     useEffect(() => {
         // getAllProblems()
         getProblemSubset(0, 50)
         console.log('Outputting problems getSubset')
     }, [getAllProblems, getProblemSubset])
 
-    
+    const handleChange = (event) => {
+        // TODO: Set this up so it triggers auto-submit after say, 3sec of no change
+        console.log('Calling handle change')
+        setSearchTerm(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        console.log('calling handle submit')
+        event.preventDefault()
+
+    }
 
     let probs = null
     if (problems) {
@@ -35,6 +50,13 @@ export const ProblemViewer = (props) => {
 
     return (
         <div>
+            <form>
+                <label>
+                    Search for a problem:
+                    <input type="text" name="name" value={searchTerm} onChange={handleChange}/>
+                </label>
+                <input type="submit" value="Submit" onSubmit={handleSubmit}/>
+            </form>
             {probs}
         </div>
     )
